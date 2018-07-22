c9008f892fd6d73698e8089be7e2624634089133
AlexYIN <AlexYIN@users.noreply.github.com>
2018-07-22 12:44:17 +0800

    Create mutex.h

---
+++ mutex.h
@@ -0,0 +1,67 @@
+#ifndef _BASE_MUTEX_H
+#define _BASE_MUTEX_H
+
+#include <pthread.h>
+
+namespace SYS
+{
+
+class MutexLock 
+{
+ public:
+  MutexLock()
+  {
+    pthread_mutex_init(&mutex_, NULL);
+  }
+
+  ~MutexLock()
+  {
+    pthread_mutex_destroy(&mutex_);
+  }
+
+  void lock()
+  {
+    pthread_mutex_lock(&mutex_);
+  }
+
+  void unlock()
+  {
+    pthread_mutex_unlock(&mutex_);
+  }
+
+  pthread_mutex_t* getPthreadMutex() /* non-const */
+  {
+    return &mutex_;
+  }
+
+ private:
+
+  pthread_mutex_t mutex_;
+};
+
+class MutexLockGuard 
+{
+ public:
+  explicit MutexLockGuard(MutexLock& mutex) : mutex_(mutex)
+  {
+    mutex_.lock();
+  }
+
+  ~MutexLockGuard()
+  {
+    mutex_.unlock();
+  }
+
+ private:
+
+  MutexLock& mutex_;
+};
+
+}
+
+// Prevent misuse like:
+// MutexLockGuard(mutex_);
+// A tempory object doesn't hold the lock for long!
+#define MutexLockGuard(x) error "Missing guard object name"
+
+#endif  //_BASE_MUTEX_H
